88d22e8ca35b8b49ff93b10a105eda167ab41541
Christ <2654630975@qq.com>
2025-05-03 17:33:50 +0800

    test

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -23,66 +23,7 @@ public class GameController {
         System.out.println("Do restart game here");
     }//这里没有在游戏界面实现restart
 
-    public boolean doMove(int row, int col, Direction direction) {
-        if (model.getId(row, col) == 1) {
-            if (model.canMove(row, col, direction)) {
-                int nextRow = row + direction.getRow();
-                int nextCol = col + direction.getCol();
-                model.getMatrix()[row][col] = 0;
-                model.getMatrix()[nextRow][nextCol] = 1;
-                repaintBox(nextRow, nextCol, view);
-                return true;
-            }
-        }
-        if (model.getId(row, col) == 2){
-            if (model.canMove(row, col, direction)){
-                int nextRow = row + direction.getRow();
-                int nextCol = col + direction.getCol();
-                model.getMatrix()[row][col] = 0;
-                model.getMatrix()[row][col + 1] = 0;
-                model.getMatrix()[nextRow][nextCol] = 2;
-                model.getMatrix()[nextRow][nextCol + 1] = 2;
-                repaintBox(nextRow, nextCol, view);
-                return true;
-            }
-        }
-        if (model.getId(row, col) == 3){
-            if (model.canMove(row, col, direction)){
-                int nextRow = row + direction.getRow();
-                int nextCol = col + direction.getCol();
-                model.getMatrix()[row][col] = 0;
-                model.getMatrix()[row + 1][col] = 0;
-                model.getMatrix()[nextRow][nextCol] = 3;
-                model.getMatrix()[nextRow + 1][nextCol] = 3;
-                repaintBox(nextRow, nextCol, view);
-                return true;
-            }
-        }
-        if (model.getId(row, col) == 4){
-            if (model.canMove(row, col, direction)){
-                int nextRow = row + direction.getRow();
-                int nextCol = col + direction.getCol();
-                model.getMatrix()[row][col] = 0;
-                model.getMatrix()[row + 1][col] = 0;
-                model.getMatrix()[row][col + 1] = 0;
-                model.getMatrix()[row + 1][col + 1] = 0;
-                model.getMatrix()[nextRow][nextCol] = 4;
-                model.getMatrix()[nextRow + 1][nextCol] = 4;
-                model.getMatrix()[nextRow][nextCol + 1] = 4;
-                model.getMatrix()[nextRow + 1][nextCol + 1] = 4;
-                repaintBox(nextRow, nextCol, view);
-                return true;
-            }
-        }
-        return false;
-    }
-    public static void repaintBox(int nextRow, int nextCol, GamePanel view){
-        BoxComponent box = view.getSelectedBox();
-        box.setRow(nextRow);
-        box.setCol(nextCol);
-        box.setLocation(box.getCol() * view.getGRID_SIZE() + 2, box.getRow() * view.getGRID_SIZE() + 2);
-        box.repaint();
-    }
+
 
     //todo: add other methods such as loadGame, saveGame...
 
