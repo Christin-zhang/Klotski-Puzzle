036026d50fac5bdebc4dfa3cdfbc4f06b4c2ccdc
Christ <2654630975@qq.com>
2025-05-18 18:41:38 +0800

    实现了比较粗糙的task3

--- src/model/MapModel.java
+++ src/model/MapModel.java
@@ -79,4 +79,12 @@ public class MapModel {
         return (newId == 0);
     }
 
+    public boolean isCaoCaoAtExit(){
+        if (matrix[4][1] == 4 && matrix[4][2] == 4) {
+            System.out.println("You win!");
+            return true;
+        }
+        else return false;
+    }
+
 }
